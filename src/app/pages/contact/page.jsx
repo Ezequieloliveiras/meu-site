f27e16bb4f11fac0c7c4abcb86c9da3590b63f2f
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-04-16 23:57:42 -0300

    criando secoes da home

--- src/app/pages/contact/page.jsx
+++ src/app/pages/contact/page.jsx
@@ -1,10 +1,9 @@
 
-import styles from '../../page.module.css'
 
 export default function Contact() {
   return (
- <div>
-  <h1>Contato</h1>
- </div>
+    <div>
+      <h1>Contato</h1>
+    </div>
   );
 }
